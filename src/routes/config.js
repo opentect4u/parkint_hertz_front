df97f295b50ca11418477abf44e317e2d419d072
Utsab Roy <roy.utsab01@gmail.com>
2026-07-21 17:26:47 +0530

    21072026-222

--- src/routes/config.js
+++ src/routes/config.js
@@ -1,5 +1,7 @@
 
 // export const BASE_URL_V5 = "http://192.168.1.33:3003/v8/api"; // Local Sayantika
-export const BASE_URL_V5 = "https://vpark.hertzcommunications.com/v8/api"; // Live
+// export const BASE_URL_V5 = "https://vpark.hertzcommunications.com/v8/api"; // Live
+
+export const BASE_URL_V5 = "https://hertz.synergicdatabank.com/v8/api"; // Live New Server
 // export const BASE_URL_V5 = "http://14.192.17.108:3003/v8/api"; // UAT
 
